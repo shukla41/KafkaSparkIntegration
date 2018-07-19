5225c559e27fca6ddb2d417ec1fc5001b7b02720
shuvamoy1983 <shuvamoy1983@gmail.com>
2018-07-20 01:27:14 +0530

    Added Kafka Producer Code

---
+++ src/main/scala/ScalaFrameWorkForSpark/ReadFile/ReadFileFromDirectory.scala
@@ -0,0 +1,25 @@
+package ScalaFrameWorkForSpark.ReadFile
+
+import java.io.File
+
+/**
+  * Created by shuvamoymondal on 7/20/18.
+  */
+object ReadFileFromDirectory {
+
+  def getListOfFiles(dir: String):List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+
+  def delete(file: File) {
+    if (file.isDirectory)
+      Option(file.listFiles).map(_.toList).getOrElse(Nil).foreach(delete(_))
+    file.delete
+  }
+
+}
