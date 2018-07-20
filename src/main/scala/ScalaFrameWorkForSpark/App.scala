7df527744800283d08930420b808fb914c859f3a
shuvamoy1983 <shuvamoy1983@gmail.com>
2018-07-20 23:12:31 +0530

    Added code Producer

--- src/main/scala/ScalaFrameWorkForSpark/App.scala
+++ src/main/scala/ScalaFrameWorkForSpark/App.scala
@@ -1,5 +1,6 @@
 package ScalaFrameWorkForSpark
 
+
 /**
  * Hello world!
  *
@@ -11,61 +12,15 @@ import org.apache.kafka.clients.producer._
 import scala.io.Source
 import scala.util.Random
 import java.io.File
+import ReadFile._
 
 object App {
   def main(args: Array[String]): Unit = {
 
-
-    val props = new Properties()
-
-    props.put("bootstrap.servers", "localhost:9092")
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-
-
-    val topic = "test3"
-    val messagesPerSec = 1
-    val wordsPerMessage = 15
-    var lines =""
-    val random = new Random()
-
-    //val names = Seq("aaaa", "bbbbb", "cccccc", "ddddd", "eeeee", "fffff", "zz", "yy", "cc", "pp", "oo", "uu", "ss", "ll");
-
-
-    //print(lines)
-    /*for(i<- 1 to 5){
-      val record = new ProducerRecord(TOPIC, "key", s"hello $i")
-      println("record is" +record)
-      producer.send(record)
-    }*/
-
-    val producer = new KafkaProducer[String, String](props)
-
+    ReadFile.KafkaProducer.KafkaConfigurationSetUp
     while (true) {
-
-
-      val files = ReadFileFromDirectory.getListOfFiles("/usr/local/src/file")
-      for (name <- files) {
-        lines = Source.fromFile(name.toString).getLines.mkString
-        ReadFileFromDirectory.delete(name)
-
-
-        //(1 to 1).foreach { messageNum =>
-
-        val str = (1 to 1).map(x => lines).mkString(" ")
-
-        println(str)
-
-        val message = new ProducerRecord[String, String](topic, null, str)
-
-        producer.send(message)
-
-        // }
-        lines = ""
-        Thread.sleep(10000)
-      }
+      ReadFile.KafkaProducer.KafkaProducerJob("test3", "/usr/local/src/file")
+      Thread.sleep(10000)
     }
-
-
   }
 }
---
+++ src/main/scala/ScalaFrameWorkForSpark/ReadFile/KafkaProducer.scala
@@ -0,0 +1,45 @@
+package ScalaFrameWorkForSpark.ReadFile
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+import scala.io.Source
+import scala.util.Random
+
+/**
+  * Created by shuvamoymondal on 7/20/18.
+  */
+object KafkaProducer {
+
+  var lines =""
+
+  def KafkaConfigurationSetUp = {
+    val props = new Properties()
+
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    val producer = new KafkaProducer[String, String](props)
+    producer
+  }
+
+
+  def KafkaProducerJob (topic_name: String, Path: String) = {
+    val topic = topic_name
+    println("Path", Path)
+      val files = ReadFileFromDirectory.getListOfFiles(Path)
+      for (name <- files) {
+        lines = Source.fromFile(name.toString).getLines.mkString("\n")
+        println("value is " + lines)
+        ReadFileFromDirectory.delete(name)
+        val str = (1 to 1).map(x => lines).mkString
+        val message = new ProducerRecord[String, String](topic, null, str)
+        KafkaProducer.KafkaConfigurationSetUp.send(message)
+        lines = ""
+
+
+    }
+  }
+
+}
